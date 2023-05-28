a77a1d1c02702bea3d1bca0444d961adeff88129
jeong-hyeran <133084927+jeong-hyeran@users.noreply.github.com>
2023-05-28 13:03:23 +0900

    달팽이 이차배열

--- day10/src/nestedArray/Quiz1_1.java
+++ day10/src/nestedArray/Quiz1_1.java
@@ -13,10 +13,12 @@ public class Quiz1_1 {
 	
 	public static void main(String[] args) {
 		
-		int size = 9;
-//		int num = 1;
+		int size = 5;
+		int num = 1;
 		int[][] arr = new int[size][size];
-		
+//		int sign = -1;
+		int line = 0;
+		int can = 0;
 		// 다음 형태로 출력하세요
 		//	1  2  3  4  5
 		//  16 17 18 19 6
@@ -24,14 +26,34 @@ public class Quiz1_1 {
 		//  14 23 22 21 8
 		//  13 12 11 10 9
 		
-		show(arr);
-		int n1 =5;
-		while(true) {
-		for (int j = 0; j < n1; j++) {
-			
-			}		
-		}
 //		show(arr);
+		
+		while(size>0) {
+			
+			for (int j = 0; j < size; j++) {
+			arr[line][can]=num;
+			can++;	num++;						// 왜 can이 5까지 가지?
+			}
+			size--; can--; line++;		
+			
+			for (int i = 0; i < size; i++) {	// size=4
+			arr[line][can]=num;			// line =1 can = 4
+			line++;	num++;					// line  = 4
+			}								// 
+			can--;line--;
+			for (int j = 0; j < size; j++) {	//size = 4
+				arr[line][can]=num;				// line = 4 can =3
+				can--; num++;					// line = 4 can = 0
+			}
+			size--;  line--; can++;			// size = 3 line = 3
+			
+			for (int i = 0; i < size; i++) {	//size = 3
+				arr[line][can]=num;			// line = 3 can = 0
+				line--; num++;				// line = 1
+			}
+			can++; line++;
+		}
+		show(arr);
 	}
 
 }
